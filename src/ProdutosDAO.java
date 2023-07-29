8950df5d7ddda1bfde7cb50c552929d86685feed
Marjorie-Godoy <godoymarjorie84@gmail.com>
2023-07-29 17:17:27 -0300

    atualiza status de um produto para vendido

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -97,4 +97,35 @@ public class ProdutosDAO {
             return null;
         }
     }
+
+    public int venderProduto(int id) {
+
+        int status;
+
+        try {
+
+            conexao.connectDB();
+            Connection conn = conexao.getConexao();
+
+            String produtovendido = "Vendido";
+
+            PreparedStatement st = conn.prepareStatement("UPDATE produtos SET status =? WHERE id =? ");
+
+            st.setInt(2, id);
+            st.setString(1, produtovendido);
+
+            status = st.executeUpdate();
+
+            return status;
+
+        } catch (SQLException ex) {
+
+            System.out.println(ex.getErrorCode());
+
+            return ex.getErrorCode();
+
+        }
+
+    }
+
 }
